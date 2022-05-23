be3e59eec5e952eed99f9dd05c38f1ea2b9629e4
gabripo <gabripo93+github@gmail.com>
2022-05-23 19:29:17 +0200

    Basic setup of setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='spacesimlib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Space Simulator Library',
+    author='Michela Salinaro, Gabriele Giardino',
+    license='MIT',
+)
